d0e5232f01a8666d182f3d89f9c2d06d18682372
Renardev <renardev@outlook.com>
2023-06-25 23:40:12 -0700

    Renardev :)

--- src/Parts/Home.jsx
+++ src/Parts/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
             <div className='home text-center'>
                 <img className='mtal-logo my-4' src={mtal} alt="MTAL Logo" />
                 <h1>YKS Sözlüğe Hoş Geldiniz!</h1>
-                <h4>Bu sözlük, YKS yoğunluklu kelimeleri içermektedir. Öğrenciye yönelik gerçekleştirilmiş bu proje Mümtaz Turhan Proje Anadolu Lisesi tarafından TÜBİTAK adına yapılmıştır. Unutmayın, sözcüklerin tanımları ve örnekleri için TDK sözlüğü kullanılmaktadır.</h4>
+                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla magnam earum repellendus ducimus saepe, eaque alias? Nisi deserunt quae fuga optio libero, non doloremque voluptatum, blanditiis magni culpa saepe facere.</h4>
             </div>
         )
     }
